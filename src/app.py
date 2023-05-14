4e33b03af126836266ed483a58b1db38dcc58012
Surya <git@sdan.io>
2023-05-13 21:41:37 -0700

    flask

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
-import json
+from flask import Flask, request, Response, send_file, render_template, render_template_string
+from flask_cors import CORS
+from utils.utils import download_pdf
+from utils.process import load_file, query_file, check_pdf_exists
+from werkzeug.exceptions import BadRequest
 import os
 import logging
 from dotenv import load_dotenv
-from quart import Quart, request, Response, send_file, render_template, render_template_string
-import quart_cors
-from utils.utils import download_pdf
-from utils.process import load_file, query_file, check_pdf_exists
+import json
 
 load_dotenv()
 
@@ -14,12 +15,13 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-app = quart_cors.cors(Quart(__name__))
+app = Flask(__name__)
+CORS(app)
 
-@app.post("/pdf/load")
-async def load_pdf():
+@app.route("/pdf/load", methods=['POST'])
+def load_pdf():
     try:
-        data = await request.get_json(force=True)
+        data = request.get_json(force=True)
         logging.info(f"[load_pdf] Data: {data}")
         pdf_url = data["pdf_url"]
 
@@ -38,10 +40,10 @@ async def load_pdf():
         return Response(response=json.dumps({"error": "An error occurred."}), status=500)
 
 
-@app.post("/pdf/<pdf_name>/query")
-async def query_pdf(pdf_name):
+@app.route("/pdf/<pdf_name>/query", methods=['POST'])
+def query_pdf(pdf_name):
     try:
-        data = await request.get_json(force=True)
+        data = request.get_json(force=True)
         query = data["query"]
         logging.info(f"[query_pdf] Querying PDF {pdf_name} with query: {query}")
         results = query_file(pdf_name, query)
@@ -53,44 +55,42 @@ async def query_pdf(pdf_name):
         logging.error(f"[query_pdf] Error occurred: {e}")
         return Response(response=json.dumps({"error": "An error occurred."}), status=500)
 
-@app.get("/logo.png")
-async def plugin_logo():
-    filename = os.path.join(current_dir, '..', 'assets', 'logo.png')
-    return await send_file(filename, mimetype='image/png')
+@app.route("/logo.png", methods=['GET'])
+def plugin_logo():
+    filename = os.path.join(current_dir, '..', 'assets')
+    return send_file(filename, 'logo.png', mimetype='image/png')
 
-@app.get("/.well-known/ai-plugin.json")
-async def plugin_manifest():
+@app.route("/.well-known/ai-plugin.json", methods=['GET'])
+def plugin_manifest():
     host = request.headers['Host']
     with open(os.path.join(current_dir, '..', 'config', 'ai-plugin.json')) as f:
         text = f.read()
         text = text.replace("PLUGIN_HOSTNAME", f"http://{host}")
         return Response(text, mimetype="text/json")
 
-@app.get("/openapi.yaml")
-async def openapi_spec():
+@app.route("/openapi.yaml", methods=['GET'])
+def openapi_spec():
     host = request.headers['Host']
     with open(os.path.join(current_dir, '..', 'config', 'openapi.yaml')) as f:
         text = f.read()
         text = text.replace("PLUGIN_HOSTNAME", f"http://{host}")
         return Response(text, mimetype="text/yaml")
-    
 
-@app.get("/legal")
-async def legal():
+@app.route("/legal", methods=['GET'])
+def legal():
     with open(os.path.join(current_dir, '..', 'legal.txt')) as f:
         return Response(f.read(), mimetype="text/plain")
 
-@app.get("/health")
-async def health_check():
+@app.route("/health", methods=['GET'])
+def health_check():
     logging.info("Health check endpoint accessed")
     return Response(response="🫡", status=200, mimetype="text/plain")
 
-@app.get("/")
-async def homepage():
+@app.route("/", methods=['GET'])
+def homepage():
     with open(os.path.join(current_dir, '..', 'index.html'), 'r') as f:
         content = f.read()
-    return await render_template_string(content)
+    return render_template_string(content)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
-
+    app.run(host="0.0.0.0", port=8000)
