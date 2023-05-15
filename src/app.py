fccb5793e7e63d85fc3d1d2c4d7264f7fc967d39
Surya <git@sdan.io>
2023-05-14 17:03:25 -0700

    update logo serve

--- src/app.py
+++ src/app.py
@@ -57,8 +57,8 @@ def query_pdf(pdf_name):
 
 @app.route("/logo.png", methods=['GET'])
 def plugin_logo():
-    filename = os.path.join(current_dir, '..', 'assets')
-    return send_file(filename, 'logo.png', mimetype='image/png')
+    filename = os.path.join(current_dir, '..', 'assets', 'logo.png')
+    send_file(filename, mimetype='image/png')
 
 @app.route("/.well-known/ai-plugin.json", methods=['GET'])
 def plugin_manifest():
