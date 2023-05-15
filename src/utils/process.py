b6837eef19853a6ffbbd08ad0618ee50531f0274
Surya <git@sdan.io>
2023-05-14 17:07:50 -0700

    chroma persist to dir

--- src/utils/process.py
+++ src/utils/process.py
@@ -39,6 +39,7 @@ client = chromadb.Client(Settings(
     chroma_server_ssl_enabled=True,
     chroma_server_http_port=443,
     chroma_db_impl="duckdb+parquet",
+    persist_directory=os.getenv("CHROMA_PERSIST_DIRECTORY", "/tmp/chroma"),
 ))
 
 # Set up embedding function
